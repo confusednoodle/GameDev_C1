532144208986321fb2099094b3a1d40d5f973f1d
miampf <miampf@proton.me>
2023-06-23 02:33:26 +0200

    bg music now fades out

--- Assets/Scripts/BgMusicHandler.cs
+++ Assets/Scripts/BgMusicHandler.cs
@@ -23,7 +23,7 @@ public class BgMusicHandler : MonoBehaviour
         while (SharedState.BgMusicVolume > 0)
         {
             SharedState.BgMusicVolume -= 0.05f;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.075f);
         }
     }
 }
